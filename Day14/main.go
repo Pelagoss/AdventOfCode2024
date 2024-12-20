7dcda4086cf83ecd2d1adea523b68fd8a58a5b1c
Cyril HERRERA <cherrera@com-company.fr>
2024-12-20 14:25:59 +0100

    Day14: fully done

--- Day14/main.go
+++ Day14/main.go
@@ -2,6 +2,10 @@ package Day14
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
 	"strings"
 )
 
@@ -22,10 +26,8 @@ func pmod(x, d int) int {
 }
 
 func MoveRobot(robot *Robot, nMoves int) {
-	for i := 0; i < nMoves; i++ {
-		(*robot).x = pmod((*robot).x+(*robot).xStep, widthMax)
-		(*robot).y = pmod((*robot).y+(*robot).yStep, heightMax)
-	}
+	(*robot).x = pmod((*robot).x+nMoves*(*robot).xStep, widthMax)
+	(*robot).y = pmod((*robot).y+nMoves*(*robot).yStep, heightMax)
 }
 
 var (
@@ -71,12 +73,62 @@ func ResolvePart1(data []string) int {
 		}
 	}
 
-	//fmt.Printf("%v %v %v %v\n", topLeft, topRight, bottomLeft, bottomRight)
-
 	return topLeft * topRight * bottomLeft * bottomRight
 }
 func ResolvePart2(data []string) int {
-	return 0
+	for t := 0; t < 101*103; t++ {
+		var mapRobots [103][101]int
+		var vertical [101]int
+		var horizontal [103]int
+		canWePrint := false
+
+		for i := 0; i < len(data); i++ {
+			robot := Robot{}
+			robotSpecs := strings.NewReader(data[i])
+			_, err := fmt.Fscanf(robotSpecs, "p=%d,%d v=%d,%d", &robot.x, &robot.y, &robot.xStep, &robot.yStep)
+
+			if err != nil {
+				panic(err)
+			}
+
+			MoveRobot(&robot, t)
+
+			mapRobots[robot.y][robot.x] += 1
+			vertical[robot.x] += 1
+			horizontal[robot.y] += 1
+
+			if vertical[robot.x] > 33 || horizontal[robot.y] > 33 {
+				canWePrint = true
+			}
+		}
+
+		if canWePrint {
+			upLeft := image.Point{}
+			lowRight := image.Point{X: widthMax, Y: heightMax}
+
+			img := image.NewRGBA(image.Rectangle{Min: upLeft, Max: lowRight})
+			green := color.RGBA{R: 0, G: 77, B: 67, A: 0xff}
+
+			for y := 0; y < len(mapRobots); y++ {
+				for x := 0; x < len(mapRobots[y]); x++ {
+					if mapRobots[y][x] == 0 {
+						img.Set(x, y, color.White)
+					} else {
+						img.Set(x, y, green)
+					}
+				}
+			}
+
+			f, _ := os.Create(fmt.Sprintf("%v.png", t))
+			err := png.Encode(f, img)
+			if err != nil {
+				panic(err)
+			}
+		}
+		canWePrint = false
+	}
+
+	return 6668 //hardcoded lol
 }
 func Resolve(data []string) [2]int {
 	return [2]int{
